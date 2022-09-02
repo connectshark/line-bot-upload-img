005403c61b6ab7c37b2ff474b1ce04b539099968
Chambers <bobhus394@gmail.com>
2022-09-02 14:29:40 +0800

    更新readme

--- utils/messageHandler.js
+++ utils/messageHandler.js
@@ -1,14 +1,14 @@
 const bot = require('../bot/index')
-const client = new bot.lineBot.Client(bot.config)
+const bot_client = new bot.lineBot.Client(bot.config)
 const imgur_client = require('../imgur/index')
 const { Readable } = require('stream')
 
 const imageHandler = async event => {
   const imageID = event.message.id
   const buffers = []
-  const stream = await client.getMessageContent(imageID)
+  const stream = await bot_client.getMessageContent(imageID)
   return new Promise((resolve, reject) => {
-    stream.on('data', async (chunk) => {
+    stream.on('data', chunk => {
       buffers.push(chunk)
     })
     stream.on('error', err => {
